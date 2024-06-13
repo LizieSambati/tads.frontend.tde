8360eb520a601e63c5ff13ff9945df3999b9a08f
liziesambati <liziesambati@gmail.com>
2024-06-13 10:46:23 -0300

    teste

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <header className="App-header">
+        <p>
+          Liziê testeeeeee
+        </p>
+      </header>
+      <div>
+        <a href="http://www.google.com.br">link</a>
+      </div>
+      {/* <div className='text'>
+        <img src="https://www.petz.com.br/blog/wp-content/uploads/2019/07/vida-de-gato.jpg" alt='texto alternativo' />
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
